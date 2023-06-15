576d0843ad7b9e42377df5917cbe8d9c93271d80
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-06-15 16:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let sound=["applause.mp3","boo.mp3","gasp.mp3","tada.mp3","victory.mp3","wrong.mp3"];
 let btns=document.getElementsByClassName("btn");
-let stop=document.getElementsByClassName("stop")
+let stop=document.getElementByClassName("stop")
 let audio;
 console.log(btns);
 for(i =0;i<btns.length;i++){
