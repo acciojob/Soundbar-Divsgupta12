418854150cc396ac44fd23b2def14efb83c72678
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-06-15 16:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let sound=[applause.mp3,boo.mp3,gasp.mp3,tada.mp3,victory.mp3,wrong.mp3];
+let btns=document.getElementsByClassName("btn");
+let stop=document.getElementsByClassName("stop")
+let audio;
+console.log(btns);
+for(i =0;i<btns.length;i++){
+    btns[i].addEventListener("click",()=>{
+        audio=new Audio(`sounds/${sound[i]}`)
+        audio.play();
+    });
+}
+stop.addEventListener("click",()=>{
+    audio.pause();
+});
